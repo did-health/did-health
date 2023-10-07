47de55fd29d84d8005e8e86967852949fb550d2f
did-health <richard.braman@did.healthcare>
2023-10-06 22:57:12 -0400

    resolver working maybe

--- packages/nextjs/pages/did-document.tsx
+++ packages/nextjs/pages/did-document.tsx
@@ -14,6 +14,61 @@ const DidDocumentForm = () => {
     args: [inputDID], // Pass the user-entered DID as an argument
   });
 
+  const convertToDidDocument = (resolvedDid) => {
+    if (!resolvedDid || !resolvedDid.healthDid) return null;
+
+    return {
+      "@context": "https://www.w3.org/ns/did/v1",
+      "id": `did:health:${resolvedDid.healthDid}`,
+      "verificationMethod": [
+        {
+          "id": `did:health:${resolvedDid.healthDid}#keys-1`,
+          "type": "EcdsaSecp256k1RecoveryMethod2020",
+          "controller": `did:health:${resolvedDid.healthDid}`,
+          "publicKeyBase58": resolvedDid.publicKeyBase58,
+          "threshold": {
+            "n": 5,
+            "t": 3
+          }
+        }
+      ],
+      "authentication": [
+        `did:health:${resolvedDid.healthDid}#keys-1`
+      ],
+      "assertionMethod": [
+        `did:health:${resolvedDid.healthDid}#keys-1`
+      ],
+      "capabilityInvocation": [
+        `did:health:${resolvedDid.healthDid}#keys-1`
+      ],
+      "capabilityDelegation": [
+        `did:health:${resolvedDid.healthDid}#keys-1`
+      ],
+      "keyAgreement": [
+        {
+          "id": `did:health:${resolvedDid.healthDid}#keys-2`,
+          "type": "EcdsaSecp256k1RecoveryMethod2020",
+          "controller": `did:health:${resolvedDid.healthDid}`,
+          "publicKeyBase58": resolvedDid.publicKeyBase58,
+          "threshold": {
+            "n": 5,
+            "t": 3
+          }
+        }
+      ],
+      "service": [
+        {
+          "id": `did:health:${resolvedDid.healthDid}#medical-records`,
+          "type": "SecureMedicalRecordsStore",
+          "serviceEndpoint": resolvedDid.serviceEndpoint,
+          "description": resolvedDid.description
+        }
+      ]
+    };
+  };
+
+  const didDocument = convertToDidDocument(resolvedDid);
+
   return (
   
     <div>
@@ -28,9 +83,16 @@ const DidDocumentForm = () => {
         </label>
       </form>
       <div>
-        <h2>Resolved DID Document</h2>
+        <h2>Resolved DID</h2>
         <div className="mb-4">
           <pre>{JSON.stringify(resolvedDid, null, 2)}</pre>
+          </div>
+      </div>
+
+      <div>
+        <h2>Resolved DID Document</h2>
+        <div className="mb-4">
+          <pre>{JSON.stringify(didDocument, null, 2)}</pre>
           </div>
       </div>
     </div>
